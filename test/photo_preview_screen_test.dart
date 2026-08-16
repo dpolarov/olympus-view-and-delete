46f3392bd591bec8bff5c3768b86e2eba37a7ae7
Dmitriy <65167625+dpolarov@users.noreply.github.com>
2026-08-16 08:08:38 +0200

    Make downloaded preview marker test deterministic

--- test/photo_preview_screen_test.dart
+++ test/photo_preview_screen_test.dart
@@ -1,10 +1,13 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart'
+    show TargetPlatform, debugDefaultTargetPlatformOverride;
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 import 'package:olympus_tg6_manager/screens/photo_preview_screen.dart';
 import 'package:olympus_tg6_manager/services/camera_api.dart';
+import 'package:olympus_tg6_manager/services/download_history.dart';
 import 'package:olympus_tg6_manager/services/image_cache.dart';
 import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -84,6 +87,7 @@ void main() {
   });
 
   tearDown(() async {
+    debugDefaultTargetPlatformOverride = null;
     await ImageDiskCache.instance.resetForTests();
     if (await tmp.exists()) {
       try {
@@ -250,12 +254,17 @@ void main() {
     expect(find.text('A.JPG'), findsOneWidget);
     expect(find.text('1/2'), findsOneWidget);
   });
+
   testWidgets('downloaded file shows download-done marker in preview',
       (tester) async {
+    debugDefaultTargetPlatformOverride = TargetPlatform.linux;
     final file = _file('DONE.JPG');
     SharedPreferences.setMockInitialValues({
       'download_history_v1': <String>[file.downloadHistoryKey],
     });
+
+    expect(await DownloadHistory.load(), contains(file.downloadHistoryKey));
+
     await _pumpPreview(
       tester,
       files: [file],
@@ -266,5 +275,4 @@ void main() {
 
     expect(find.byIcon(Icons.download_done), findsOneWidget);
   });
-
 }
